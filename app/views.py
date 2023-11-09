3aaa2c74222d6fc9e1cc89d52b88d53ad7a633c6
Thotakishore5172 <thotakishore5172@gmail.com>
2023-11-09 11:08:03 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def don(request):
+    return HttpResponse('hii hello')
+
+def macha(reruest):
+    return HttpResponse('Thinnavaraaa')
